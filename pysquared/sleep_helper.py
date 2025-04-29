5f817a19d57847d5909303bb388411ac38e371d3
blakejameson <lpo13@txstate.edu>
2025-04-29 18:36:36 -0500

    removing unused additions to rv3028manager and replace alarm type in sleep_helper

--- pysquared/sleep_helper.py
+++ pysquared/sleep_helper.py
@@ -2,8 +2,6 @@ import gc
 import time
 
 import alarm
-from alarm import time as alarmTime
-from alarm.time import TimeAlarm
 
 from .config.config import Config
 from .logger import Logger
@@ -54,7 +52,7 @@ class SleepHelper:
         while time_remaining > 0:
             time_increment = time_remaining if time_remaining < 15 else 15
 
-            time_alarm: circuitpython_typing.Alarm = alarm.time.TimeAlarm(
+            time_alarm: alarm.time.TimeAlarm = alarm.time.TimeAlarm(
                 monotonic_time=time.monotonic() + time_increment
             )
             alarm.light_sleep_until_alarms(time_alarm)
