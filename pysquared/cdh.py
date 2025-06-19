ff38cfa2a93403127ece79883b29f53099b1ff02
Nate Gay <email@nategay.me>
2025-06-19 18:03:37 -0500

    cdh Exception to str in message sent to ground

--- pysquared/cdh.py
+++ pysquared/cdh.py
@@ -86,7 +86,7 @@ class CommandDataHandler:
         except Exception as e:
             self._log.error("Failed to process command message", err=e)
             self._packet_manager.send(
-                f"Failed to process command message: {e}".encode("utf-8")
+                f"Failed to process command message: {str(e)}".encode("utf-8")
             )
             return
 
