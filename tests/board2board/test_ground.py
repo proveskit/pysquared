7524f477b21faea388f5ac8ede71418f9402537a
ineskhou <ik2512@columbia.edu>
2025-07-18 16:33:00 -0700

    changed so takes from file so we dont mess up message q. Both boards work

--- tests/board2board/test_ground.py
+++ tests/board2board/test_ground.py
@@ -6,25 +6,23 @@ import serial
 # import serial.tools.list_ports
 import os
 
-#commening out because I am just testing the flight controller board
-# @pytest.fixture
-# def GRstation():
-#     ground_path = "Volumes/CIRCUITPY/"
-#     ground_port = "/dev/cu.usbmodem14201" #should not be hardcoded    
-#     #serial connect
-#     ground = FCBoard(ground_path, ground_port)
+@pytest.fixture
+def GRstation():
+    ground_path = "Volumes/CIRCUITPY/"
+    ground_port = "/dev/cu.usbmodem14101" #should probably not be hardcoded    
+    #serial connect
+    ground = FCBoard(ground_path, ground_port)
 
-#     ground.get_into_repl()
+    ground.get_into_repl()
 
-#     t = threading.Thread(target=telemetry_logger, args=(ground), daemon=True)           
-#     t.start()
+    t = threading.Thread(target=telemetry_logger, args=(ground, "ground_telemetry.log"), daemon=True)           
+    t.start()
 
-#     #time.sleep(10)
-
-#     yield ground
-#     #runs after the test is done
-#     ground.stop_reader_thread()
-    # ground.ser.close()
+    yield ground
+    #runs after the test is done
+    ground.stop_reader_thread()
+    ground.ser.close()
+    t.join(timeout=2)
 
 #currently logs to a file, but should be able to log to Github through Actions later.
 def telemetry_logger(fc, file:str="fc_telemetry.log"):
@@ -41,23 +39,34 @@ def FC_Board():
     board = FCBoard(board_path, board_port)
     board.soft_reset() #should do control C, wait and control D
     #start the telemetry logger thread.
-    t = threading.Thread(target=telemetry_logger, args=(board,), daemon=True)           
+    t = threading.Thread(target=telemetry_logger, args=(board, "fc_telemetry.log"), daemon=True)           
     t.start()
     yield board
     board.stop_reader_thread()
     board.ser.close()
     t.join(timeout=2)
 
-# def test_connect_to_ground_station(GRstation):
-#     time.sleep(1)
-#     print(GRstation.get_all_messages()) # need to wait for past commands to be on board so we can clear them out
-#     time.sleep(1) # need to wait for command to be on board
-#     GRstation.get_message() # this should be print('Hello, World!') That I called above.
-#     assert GRstation.get_message() == "Hello, World!"
+def test_connect_to_ground_station(GRstation):
+    time.sleep(1)
+    # Get current line count before sending command
+    with open("ground_telemetry.log", "r") as f:
+        lines_before = len(f.readlines())
+        
+    print("Lines before:", lines_before)
+    time.sleep(1) # need to wait for command to be on board
+    GRstation.send_command_to_board("print('Hello, World!')")
+    time.sleep(1)
+    # Read only new lines from the telemetry log file
+    with open("ground_telemetry.log", "r") as f:
+        all_lines = f.readlines()
+        new_lines = all_lines[lines_before-2:]  # -1 for the first line, -1 for the >>> 
+        
+    assert "Hello, World!\n" in new_lines
 
 #the FC board should be reading, not executing. so we just want to make sure we are connected to it.
 def test_connect_to_fc_board(FC_Board):
-    assert FC_Board.get_all_messages() is not None
+    with open("fc_telemetry.log", "r") as f:
+        assert f.read() is not None
 
 
 def test_beacon_format():
