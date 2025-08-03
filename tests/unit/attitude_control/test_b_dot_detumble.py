945b0ffe4b3bf0d37525c18aadaf193efdb22f04
Nate Gay <email@nategay.me>
2025-08-02 20:57:41 -0500

    Scaffold tests

---
+++ tests/unit/attitude_control/test_b_dot_detumble.py
@@ -0,0 +1,21 @@
+"""Unit tests for the detumble module.
+
+This module contains unit tests for the `detumble` module, which provides
+functions for spacecraft detumbling. The tests cover dot product, cross product,
+and magnetorquer dipole calculations.
+"""
+
+
+def test_magnitude():
+    """Tests the _magnitude function."""
+    pass
+
+
+def test_dB_dt():
+    """Tests the _dB_dt function."""
+    pass
+
+
+def test_dipole_moment():
+    """Tests the dipole_moment function."""
+    pass
