8f35601b20cd94f9073d439c4bd67ec8901be019
Nate Gay <email@nategay.me>
2025-09-01 18:13:29 -0500

    Remove unused mocks

--- mocks/circuitpython/storage.py
+++ mocks/circuitpython/storage.py
@@ -7,19 +7,6 @@ module without the need for actual hardware.
 
 from __future__ import annotations
 
-from circuitpython_typing import BlockDevice
-
-
-def mount(filesystem: VfsFat, mount_path: str, *, readonly: bool = False) -> None:
-    """A mock function to mount the filesystem.
-
-    Args:
-        filesystem: The filesystem to mount.
-        mount_path: Where to mount the filesystem.
-        readonly: True when the filesystem should be readonly to CircuitPython.
-    """
-    pass
-
 
 def disable_usb_drive() -> None:
     """A mock function to disable the USB drive."""
@@ -39,11 +26,3 @@ def remount(path: str, readonly: bool) -> None:
         readonly: Whether to mount as read-only.
     """
     pass
-
-
-class VfsFat:
-    """A mock class representing a VfsFat filesystem."""
-
-    def __init__(self, block_device: BlockDevice) -> None:
-        """Initializes the mock VfsFat filesystem."""
-        pass
